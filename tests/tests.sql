e1a88a6ef960d48c6e2e5909bca5b5240231cf06
naseer9177596190-hash <naseer9177596190@gmail.com>
2026-07-27 12:37:40 +0530

    Enhance tests.sql with data validation queries
    Added SQL queries to check for nulls, duplicates, and data integrity in product information.

--- tests/tests.sql
+++ tests/tests.sql
@@ -24,6 +24,43 @@ select
 distinct cst_gndr 
 from silver.crm_cust_info
 
+
+
+  -- Test for silver.crm.prd_info 
+
+
+
+
+
+
+
+--- checking if prd_id has any null or duplicates
+SELECT prd_id,
+count(*) as prd_id_count from bronze.crm_prd_info
+group by prd_id
+having count(*) > 1 or prd_id is null;
+
+
+-- Check if prd_nm has any unwanted spaces
+
+SELECT prd_nm
+from bronze.crm_prd_info
+where prd_nm != Trim(prd_nm)
+
+-- Check of prd_cost has any null 
+
+SELECT prd_cost
+from bronze.crm_prd_info
+where prd_cost IS NULL;
+
+
+
+-- Lets check if the start date is less than end date
+
+
+SELECT * from bronze.crm_prd_info
+where prd_start_dt < prd_end_dt;
+
 -- lets check values in maritalstatus column
 select 
 distinct cst_marital_status 
